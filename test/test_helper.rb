77bfc9983826af06f06155755d7739538000207e
Alexey Palazhchenko <alexey.palazhchenko@qik.com>
2010-09-23 09:50:09 +0400

    Add patch to detect slow tests.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,3 +11,36 @@ def get_response(name)
   f.close
   return retval
 end
+
+
+# patch to detect slow tests
+
+$_tests_run_times = {}
+
+class ActiveSupport::TestCase
+  setup :_start_timer
+  def _start_timer
+    @_start_time = Time.now
+  end
+
+  teardown :_stop_timer
+  def _stop_timer
+    $_tests_run_times[self.to_s] = Time.now - @_start_time
+  end
+end
+
+at_exit do
+  unless $! || Test::Unit.run?  # run tests only once
+    exit_code = Test::Unit::AutoRunner.run
+
+    if $_tests_run_times.present?
+      res = $_tests_run_times.select{ |m, s| s > 1 }.sort_by(&:last).reverse[0, 20]
+      if res.present?
+        puts "\n\nTop #{res.count} slowest tests methods (including all setups and teardowns):"
+        res.each { |l| puts "#{l.last.to_s[0, 4]} - #{l.first}" }
+      end
+    end
+
+    exit exit_code
+  end
+end
