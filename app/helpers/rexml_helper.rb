c15c36673de615327b417ad44c0644f01dea399a
Alexey Palazhchenko <alexey.palazhchenko@qik.com>
2010-09-26 13:56:14 +0400

    Cleanup.

--- app/helpers/rexml_helper.rb
+++ app/helpers/rexml_helper.rb
@@ -1,14 +1,6 @@
-# $Id$
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
-
 module RexmlHelper
-
   def get_element_value(element, name)
-    return "" if element == nil
-    return "" if element.get_text(name) == nil
-    return "" if element.get_text(name).value == nil
-    return element.get_text(name).value
+    value = element.try(:get_text, name).try(:value)
+    value.present? ? value : ""
   end
-
 end
